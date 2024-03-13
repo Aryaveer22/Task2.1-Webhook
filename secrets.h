cbc2a4ecd5540e3f320d9c70d8167e060a241116
Aryaveer22 <111032260+Aryaveer22@users.noreply.github.com>
2024-03-14 01:12:59 +1100

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Avj"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 	2467134		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "I8MTYZLHT3NVQKU1"   // replace XYZ with your channel write API Key
